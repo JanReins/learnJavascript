f770d2ed372373b1b12de6aa0045cd80b3233f19
Jan Reinhold <140710185+JanReins@users.noreply.github.com>
2024-10-09 03:48:59 +0000

    second etiration

--- pyramid-generator.js
+++ pyramid-generator.js
@@ -2,8 +2,10 @@ const character = "#";
 const count = 8;
 const rows = [];
 
+
+
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(character.repeat(i));
+    rows.push(character.repeat(i + 1));
   }
 
 let result = ""
